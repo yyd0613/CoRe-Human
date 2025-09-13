f0250320492a137ba82a1f7ae7cc52f7adb18d00
yyd0613 <75132578+yyd0613@users.noreply.github.com>
2025-09-13 22:24:39 +0800

    Delete lib directory

--- lib/net/__init__.py
+++
@@ -1,6 +0,0 @@
-from .BasePIFuNet import BasePIFuNet
-from .HGPIFuNet import HGPIFuNet
-from .NormalNet import NormalNet
-from .VE import VolumeEncoder
-from .UNet import UNet
-from .HallucinatorNet import Hallucinator
--- lib/pymafx/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .data_loader import CheckpointDataLoader
-from .saver import CheckpointSaver
